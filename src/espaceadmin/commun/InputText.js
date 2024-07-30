3919a75561b0c1f938139160d3a084215c6f606e
Agnes75 <larocheagnes1440@gmail.com>
2024-07-30 15:44:42 +0200

    ajout dossier commun sur branche siteadmin

---
+++ src/espaceadmin/commun/InputText.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputText = ({label,type, name,onChange}) => {
+
+    return (
+        <div className="form-group mb-3">
+            <label htmlFor={name} className="form-label">{label}</label>
+            <input type={type} className="form-control"  id={name} name={name} onChange={onChange}/>
+        </div>
+    )
+}
+
+export default InputText
